f6e752b3ee3b3c5c45e8285f9662555dc9803a35
b1i3ll <111253761+b1i3ll@users.noreply.github.com>
2022-12-05 08:30:03 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -21,16 +21,7 @@
 #
 # Para obter a nota máxima dessa questão, deve-se utilizar apenas um ``for`` e nenhuma função pronta do Python.
 
-def q1(heights):
-    maior=0 
-    n=1
-    for x in heights
-    if x > maior:
-        maior=i
-        elif i==maior:
-            n+=1
-            return n
-        return 0
+
 
 
 if __name__ == '__main__':
