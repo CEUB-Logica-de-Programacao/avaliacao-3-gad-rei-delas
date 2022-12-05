c126c8e06aaecfce5f871c0ee87ea5dde9948552
Gabriel Gad Costa Weyers <83155013+Gaad25@users.noreply.github.com>
2022-12-05 08:33:36 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -20,7 +20,16 @@
 # Isso porque existem dois jogadores com altura máxima (180).
 #
 # Para obter a nota máxima dessa questão, deve-se utilizar apenas um ``for`` e nenhuma função pronta do Python.
+
 def q1(heights):
+    maior=0
+    n=1
+    for x in heights:
+        if x > maior:
+            maior = x
+        elif x == maior:
+            n+=1
+        return n
 
 
 
